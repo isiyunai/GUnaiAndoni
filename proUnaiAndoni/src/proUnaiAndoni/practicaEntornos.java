25decac5ab7c48af5afa8c760d1c13d427531209
jvem2 <jvem2@192.168.1.24>
2024-04-01 19:34:50 +0200

    Cambio nosecuales

--- proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
+++ proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
@@ -6,7 +6,7 @@ public class practicaEntornos {
 		System.out.println("Hola Mundo");
 		System.out.println("Adios Mundo");
 		System.out.println("Adios Mundo Cruel");
-		System.out.println("Hola Andoni");
+		System.out.println("Andoni como m,e vuelvas a pedir que me relaje te rajo");
 	}
 	
 
