24b682658545c2740db86eb2ff74c90f33a94224
andon <andon@DESKTOP-3KTR7DK>
2024-04-01 20:29:12 +0200

    Pro2 linea

--- proUnaiAndoni/src/pro2UnaiAndoni/practicaEntornos2.java
+++ proUnaiAndoni/src/pro2UnaiAndoni/practicaEntornos2.java
@@ -3,6 +3,7 @@ package pro2UnaiAndoni;
 public class practicaEntornos2 { 
 	public static void main (String [] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Unai");
 	}
 
 }
