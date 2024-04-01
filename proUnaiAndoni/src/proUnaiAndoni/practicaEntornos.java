d0ee5dae34c54cd9f8d4adc487a875dbfef08278
jvem2 <jvem2@192.168.1.24>
2024-04-01 18:58:14 +0200

    sexta linea

--- proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
+++ proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
@@ -8,6 +8,7 @@ public class practicaEntornos {
 		System.out.println("Adios Mundo Cruel");
 		System.out.println("Hola Andoni");
 		System.out.println("Conflicto con Andoni");
+		System.out.println("Conflicto sin Andoni");
 	}
 	
 
