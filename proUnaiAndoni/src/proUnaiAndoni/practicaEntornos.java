de0f7be4034a23250b45419a5bd43529846502b1
andon <andon@DESKTOP-3KTR7DK>
2024-04-01 19:10:06 +0200

    Sexto cambio

--- proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
+++ proUnaiAndoni/src/proUnaiAndoni/practicaEntornos.java
@@ -10,7 +10,7 @@ public class practicaEntornos {
 		System.out.println("Conflicto con Andoni");
 		System.out.println("Conflicto sin Andoni");
 		System.out.println("hola mundo");
-		System.out.println("·");
+	
 	}
 	
 
