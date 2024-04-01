56b47b8562821719e48b15476f8674fa986e96d7
jvem2 <jvem2@192.168.1.24>
2024-04-01 20:12:30 +0200

    Primer cambio del segundo programa

--- proUnaiAndoni/src/pro2UnaiAndoni/practicaEntornos2.java
+++ proUnaiAndoni/src/pro2UnaiAndoni/practicaEntornos2.java
@@ -1,5 +1,8 @@
 package pro2UnaiAndoni;
 
-public class practicaEntornos2 {
+public class practicaEntornos2 { 
+	public static void main (String [] args) {
+		System.out.println("Hola Mundo");
+	}
 
 }
